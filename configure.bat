c63f6fc42b353278284e944661cf2c77005c76e1
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2014-04-01 18:24:32 +0200

    Add a simple configure script for Windows

---
+++ configure.bat
@@ -0,0 +1,21 @@
+@echo off
+echo Oh, hi. Sorry to hear we are compiling on Windows...
+echo spdr is not tested on Windows as much as it is being tested on Linux, sorry
+echo .
+echo For now, only -t (--translate) flag is implemented in this script
+
+echo Generating TS files...
+rem lupdate spdr.pro
+echo Generating QM files...
+rem lrelease libspdr/libspdr.pro
+rem lrelease cli/cli.pro
+rem lrelease widgetgui/widgetgui.pro
+echo Copying QM files to destination directories:
+mkdir bin\locale
+echo Spdr library
+move libspdr\locale\*.qm bin\locale
+echo Spdr Command Line Tool
+move cli\locale\*.qm bin\locale
+echo Spdr Widget GUI
+move widgetgui\locale\*.qm bin\locale
+echo Done
